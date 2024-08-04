9588030bbd89382dbf34d74ea3053ae1b8b488a3
kevin.haffizzana <kevin.haffizzana@gojek.com>
2024-08-04 21:36:59 +0700

    fix: update test scenario with bigger load

--- cmd/post.go
+++ cmd/post.go
@@ -30,7 +30,7 @@ var postCmd = &cobra.Command{
 	Short: "Post Benchmark",
 	Long:  `Post benchmark.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		rate := vegeta.Rate{Freq: 100, Per: time.Second}
+		rate := vegeta.Rate{Freq: 200, Per: time.Second}
 		duration := 60 * 5 * time.Second
 		targeter := NewPostCustomTargeter()
 		attacker := vegeta.NewAttacker()
